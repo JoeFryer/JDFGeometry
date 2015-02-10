06c3e2b320004a5129b40573d34ecba57c8128b9
Joe Fryer <joe.d.fryer@gmail.com>
2015-02-10 18:33:40 +0000

    Added a simple demo.

--- Example/JDFGeometry/JDFViewController.m
+++ Example/JDFGeometry/JDFViewController.m
@@ -8,22 +8,33 @@
 
 #import "JDFViewController.h"
 
+// JDFGeometry
+#import "JDFGeometry.h"
+
+
 @interface JDFViewController ()
 
 @end
+
 
 @implementation JDFViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect rect1 = CGRectMake(0.0f, 0.0f, 150.0f, 150.0f);
+    rect1 = JDFCGRectCentredInRect(rect1, self.view.bounds);
+    
+    CGRect rect2 = CGRectMake(0.0f, 0.0f, 50.0f, 50.0f);
+    rect2 = JDFCGRectTopRightCornerPositionedAtTopLeftCornerOfRect(rect2, rect1);
+    
+    UIView *view1 = [[UIView alloc] initWithFrame:rect1];
+    view1.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:view1];
+    
+    UIView *view2 = [[UIView alloc] initWithFrame:rect2];
+    view2.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:view2];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
