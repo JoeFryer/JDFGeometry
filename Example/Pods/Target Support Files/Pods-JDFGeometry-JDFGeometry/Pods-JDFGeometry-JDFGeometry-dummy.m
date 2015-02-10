626dac246a9df28679fc0bfa9731f04ea84ed8eb
Joe Fryer <joe.d.fryer@gmail.com>
2015-02-10 18:21:07 +0000

    Project setup.

---
+++ Example/Pods/Target Support Files/Pods-JDFGeometry-JDFGeometry/Pods-JDFGeometry-JDFGeometry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JDFGeometry_JDFGeometry : NSObject
+@end
+@implementation PodsDummy_Pods_JDFGeometry_JDFGeometry
+@end
---
+++ Example/Pods/Target Support Files/Pods-Tests-JDFGeometry/Pods-Tests-JDFGeometry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tests_JDFGeometry : NSObject
+@end
+@implementation PodsDummy_Pods_Tests_JDFGeometry
+@end
